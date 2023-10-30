c45dbc2891e1c7e2c54d4562988bb118549b6695
dmitrynovik <cryinstone@gmail.com>
2023-10-30 17:35:34 +1100

    feat: routing

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
 using OpenMeteo;
 using WeatherWebServer.Services;
@@ -20,6 +18,7 @@ namespace WeatherWebServer.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly WeatherRepository _weatherRepository;
@@ -32,6 +31,7 @@ namespace WeatherWebServer.Controllers
         }
 
         [HttpGet]
+        [Route("")]
         public async Task<WeatherForecast> Get(double lat = -33.52, double lon = 151.12)
         {   
             var url = $"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,windspeed_10m&hourly=temperature_2m,relativehumidity_2m,windspeed_10m";
@@ -42,6 +42,8 @@ namespace WeatherWebServer.Controllers
             return response;
         }
 
+        [HttpGet]
+        [Route("forecast")]
         public WeatherForecast Get2(string location) => _weatherRepository.Get(location);
     }
 }
